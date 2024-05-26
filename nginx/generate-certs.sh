2cf208d51c8eca57dad1831f374c41a2eafff5c1
hiro1321 <>
2024-05-26 20:01:15 +0900

    SSL化の設定

--- nginx/generate-certs.sh
+++ nginx/generate-certs.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ ! -d "/etc/letsencrypt/live/openbodylog.com" ]; then
   mkdir -p /var/www/certbot
   certbot certonly --webroot --webroot-pat --email hii543424@gmail.com --agree-tos --no-eff-email --force-renewal -d openbodylog.com -d www.openbodylog.com
