ebfde42676cbe237e1211a874f41360a99fc88b1
hiro1321 <>
2024-05-27 11:49:18 +0900

    update

--- nginx/generate-certs.sh
+++ nginx/generate-certs.sh
@@ -2,5 +2,5 @@
 
 if [ ! -d "/etc/letsencrypt/live/openbodylog.com" ]; then
   mkdir -p /var/www/certbot
-  certbot certonly --webroot --webroot-pat --email hii543424@gmail.com --agree-tos --no-eff-email --force-renewal -d openbodylog.com -d www.openbodylog.com
+  certbot certonly --nginx --email hii543424@gmail.com --agree-tos --no-eff-email --force-renewal -d openbodylog.com -d www.openbodylog.com
 fi
