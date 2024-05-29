f5230fa65c6ee9943aca379cf2200fdd1808d1d0
hiro1321 <>
2024-05-29 09:37:24 +0900

    update

--- fitlogproject/urls.py
+++ fitlogproject/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("fitlogapp.urls")),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
